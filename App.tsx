cd20a9ae90790dd4c375855d35b3eb317b7c7a33
Said Alisic <saidalisic@Saids-MacBook-Pro.local>
2023-06-27 19:18:00 +0200

    refactor: 💡 (App) Move custom theme into new component

--- App.tsx
+++ App.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import { NativeBaseProvider, View, extendTheme } from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 import { DarkModeButton } from './components';
-import { LoginScreen, SettingsScreen } from './screens';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Navigation from './navigation/Navigation';
-
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: 'dark',
-};
-
-// extend the theme
-export const theme = extendTheme({ config });
-type MyThemeType = typeof theme;
-declare module 'native-base' {
-  interface ICustomTheme extends MyThemeType {}
-}
-
-const Stack = createNativeStackNavigator();
 
 export default function App(): JSX.Element {
   return (
---
+++ libs/theme/Theme.tsx
@@ -0,0 +1,16 @@
+import { extendTheme } from 'native-base';
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: 'dark',
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+
+type MyThemeType = typeof theme;
+
+declare module 'native-base' {
+  interface ICustomTheme extends MyThemeType {}
+}
