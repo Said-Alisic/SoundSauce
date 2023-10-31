59170950148306406638d6d28cb08b3961b707ec
Said Alisic <saidalisic@Saids-MacBook-Pro.local>
2023-10-31 19:21:00 +0100

    feat: 🎸 Add MusicPlayerScreen to Navigation component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import { DarkModeButton } from './components';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Navigation from './navigation/Navigation';
-import { MusicPlayerScreen } from './screens';
 
 export default function App(): JSX.Element {
   return (
     <NativeBaseProvider>
-      <MusicPlayerScreen />
-      {/* <Navigation /> */}
+      <Navigation />
       <DarkModeButton />
     </NativeBaseProvider>
   );
